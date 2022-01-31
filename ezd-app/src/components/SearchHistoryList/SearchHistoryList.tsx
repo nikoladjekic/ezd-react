af7302f9339391cd3d11a4260f0a26079bcd6730
nikoladjekic <nikola.djekic@prodyna.com>
2022-01-31 21:37:45 +0100

    add history list component

---
+++ ezd-app/src/components/SearchHistoryList/SearchHistoryList.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { List, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from "@mui/material";
+import ManageSearchIcon from "@mui/icons-material/ManageSearch";
+
+interface SearchHistoryListProps {
+  searchHistory: string[];
+}
+
+const SearchHistoryList: React.FC<SearchHistoryListProps> = ({ searchHistory }) => {
+  return (
+    <List
+      sx={{ width: "100%", maxWidth: 250, bgcolor: "ghostwhite" }}
+      aria-labelledby="search-history-subheader"
+      subheader={
+        <ListSubheader component="div" id="search-history-subheader">
+          Search History
+        </ListSubheader>
+      }
+    >
+      {searchHistory.map((value) => (
+        <ListItemButton>
+          <ListItemIcon>
+            <ManageSearchIcon />
+          </ListItemIcon>
+          <ListItemText primary={value} />
+        </ListItemButton>
+      ))}
+    </List>
+  );
+};
+
+export default SearchHistoryList;
