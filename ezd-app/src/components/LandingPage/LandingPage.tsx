ab4d74abf8897bd30bb65086506dbc80cc8b60ab
nikoladjekic <nikola.djekic@prodyna.com>
2022-01-31 18:16:45 +0100

    add our basic two empty pages for the project

---
+++ ezd-app/src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface LandingPageProps {}
+
+const LandingPage: React.FC<LandingPageProps> = () => {
+  return <div>Hello World</div>;
+};
+
+export default LandingPage;
---
+++ ezd-app/src/components/SingleFactPage/SingleFactPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface SingleFactPageProps {}
+
+const SingleFactPage: React.FC<SingleFactPageProps> = () => {
+  return <div>Hello World</div>;
+};
+
+export default SingleFactPage;
